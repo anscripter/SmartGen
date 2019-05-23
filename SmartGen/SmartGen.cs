c459ead0162543f9fdb02f4b14493e00fd8a5fdf
Anon Scripter <46980958+anscripter@users.noreply.github.com>
2019-05-23 09:01:08 -0400

    Update SmartGen.cs

--- SmartGen/SmartGen.cs
+++ SmartGen/SmartGen.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DiscordRaper.Properties;
+using YourProject.Properties;
 namespace SmartGen
 {
     public class SmartGenerator : IDisposable
